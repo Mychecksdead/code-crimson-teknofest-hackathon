c911c939e063c5f38a941dcb9027492828ab4064
G4mb1t <68869687+G4mb1t-42@users.noreply.github.com>
2022-08-05 11:41:01 +0300

    added database

--- qr_script/qr_dump/script.py
+++ qr_script/qr_dump/script.py
@@ -2,12 +2,30 @@ import pyqrcode
 import png
 from pyqrcode import QRCode
 import shutil 
+import sqlite3
+database = sqlite3.connect("qr_database.db")
+cursor = database.cursor()
+
+
+
+cursor.execute("CREATE TABLE IF NOT EXISTS kullanicilar('isim', 'soyisim','PNR','QR_id')")
+isim = input("Kullanici ismini giriniz: ")
+soyisim = input("Kullanici soyismini giriniz: ")
+pnr  = input("Kullanici pnrini giriniz: ")
+qr_id = "QR_"
+cursor.execute("SELECT isim FROM kullanicilar")
+veri = cursor.fetchall()
+sayi = len(veri)
+qr_id = qr_id + str(sayi)
 try:
     link = "text"
     url = pyqrcode.create(link)
-    url.png("qr.png", scale = 8)
-
+    url.png("{}.png".format(qr_id), scale = 8)
+    #qr olustur
 except:
     print("error")
+komut = "INSERT INTO kullanicilar VALUES('{}','{}','{}','{}')".format(isim,soyisim,pnr,qr_id)
+cursor.execute(komut)
+database.commit()
+#qrlari ekle 
 
-    ##Directory ayarlarini falan yapmadim lmao sadece olusturuyo simdilik
